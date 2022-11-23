d4d68093e1ea926cb6a3b1346fa824c4bf3cd8fe
Philippe Martens <Palmartens@ziggo.nl>
2022-11-23 12:15:22 +0100

    Add services p2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -9,7 +9,7 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   });
-
+  
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -12,8 +14,8 @@ export class HeroesComponent {
   heroes: Hero[] = [];
 
   selectedHero?: Hero;
-  
-  constructor(private heroService: HeroService) {}
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -21,6 +23,7 @@ export class HeroesComponent {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
 
   getHeroes(): void {
